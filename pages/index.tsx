43ea092e93cad221ac5d12bc39c8c1ec25322007
Jverg <giannisverg@gmail.com>
2023-02-01 09:19:56 +0200

    First approach for infinite loading

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,30 +1,62 @@
 import React, { useEffect, useState } from 'react';
 import listJobs from '@services/jobs/jobs-http.service';
 import JobsList from '@components/jobs-list/JobsList';
-import { Spin } from 'antd';
+import { Image, Spin } from 'antd';
 import Jobs, { JobModel } from '@models/jobs';
+import { useInView } from 'react-intersection-observer';
 import styles from './index.module.less';
 
 const HomePage: React.FC = () => {
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
   const [jobs, setJobs] = useState<Array<JobModel>>();
 
-  const fetchJobs = async () => {
-    try {
-      const jobsList: Jobs = await listJobs();
-      setJobs(jobsList.items);
-    } catch (requestError: any) {
-      console.log('Fail');
+  const [ref, inView] = useInView({
+    threshold: 0.5,
+  });
+
+  const fetchJobs = async (pageNum: number) => {
+    if (page === 1 || page <= totalPages) {
+      try {
+        const jobsList: Jobs = await listJobs(pageNum);
+        if (jobs) {
+          setJobs([...jobs, ...jobsList.items]);
+        } else {
+          setJobs(jobsList.items);
+        }
+        setTotalPages(jobsList.totalPages);
+      } catch (requestError: any) {
+        console.log('Fail');
+      }
+    }
+  };
+
+  const handleScroll = () => {
+    if (inView) {
+      setPage(page + 1);
     }
   };
 
   useEffect(() => {
-    fetchJobs();
-  }, []);
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  });
+
+  useEffect(() => {
+    fetchJobs(page);
+  }, [page]);
 
   return jobs ? (
     <div style={{ display: 'flex', justifyContent: 'center', flexDirection: 'column' }}>
       <JobsList jobs={jobs} />
-      <div className={`h6 ${styles.loadingMore}`}>loading more jobs</div>
+      {page < totalPages && (
+        <div ref={ref} className={`h6 ${styles.loadingMore}`}>
+          <Image preview={false} src="/assets/svgs/loading-dot-icon.svg" alt="Loading dots" />
+          <div className={styles.loadingText}>loading more jobs</div>
+        </div>
+      )}
     </div>
   ) : (
     <Spin />
